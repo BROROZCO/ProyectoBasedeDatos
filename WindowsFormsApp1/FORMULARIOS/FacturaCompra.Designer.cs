c6e322632048280d9d5b214fdd6709d1c5422d04
Invisible <unenes.viejos@gmail.com>
2021-06-30 14:49:10 -0600

    Factura Compra Update Listo Falta Delete

--- WindowsFormsApp1/FORMULARIOS/FacturaCompra.Designer.cs
+++ WindowsFormsApp1/FORMULARIOS/FacturaCompra.Designer.cs
@@ -30,23 +30,27 @@ namespace WindowsFormsApp1.FORMULARIOS
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.datagridFacturaCompra = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCodigoEmpleado = new System.Windows.Forms.TextBox();
+            this.txtNumeroFactura = new System.Windows.Forms.TextBox();
+            this.txtYEAR = new System.Windows.Forms.TextBox();
+            this.txtDetalleCompra = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.datagridFacturaCompra = new System.Windows.Forms.DataGridView();
+            this.txtMES = new System.Windows.Forms.TextBox();
+            this.txtDIA = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.datagridFacturaCompra)).BeginInit();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.datagridFacturaCompra)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -54,7 +58,7 @@ namespace WindowsFormsApp1.FORMULARIOS
             this.panel1.Controls.Add(this.groupBox1);
             this.panel1.Controls.Add(this.button4);
             this.panel1.Controls.Add(this.button3);
-            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.btnModificar);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.datagridFacturaCompra);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -63,112 +67,26 @@ namespace WindowsFormsApp1.FORMULARIOS
             this.panel1.Size = new System.Drawing.Size(800, 450);
             this.panel1.TabIndex = 0;
             // 
-            // datagridFacturaCompra
-            // 
-            this.datagridFacturaCompra.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.datagridFacturaCompra.Location = new System.Drawing.Point(12, 191);
-            this.datagridFacturaCompra.Name = "datagridFacturaCompra";
-            this.datagridFacturaCompra.Size = new System.Drawing.Size(776, 184);
-            this.datagridFacturaCompra.TabIndex = 0;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(12, 390);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(124, 37);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(230, 390);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(124, 37);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(424, 390);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(124, 37);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(664, 390);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(124, 37);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtDIA);
+            this.groupBox1.Controls.Add(this.txtMES);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.textBox3);
-            this.groupBox1.Controls.Add(this.textBox4);
-            this.groupBox1.Controls.Add(this.textBox2);
-            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.txtCodigoEmpleado);
+            this.groupBox1.Controls.Add(this.txtNumeroFactura);
+            this.groupBox1.Controls.Add(this.txtYEAR);
+            this.groupBox1.Controls.Add(this.txtDetalleCompra);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(776, 173);
             this.groupBox1.TabIndex = 5;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "DATOS";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(516, 50);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(242, 20);
-            this.textBox1.TabIndex = 0;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(516, 117);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(242, 20);
-            this.textBox2.TabIndex = 1;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(119, 117);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(242, 20);
-            this.textBox3.TabIndex = 3;
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(119, 50);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(242, 20);
-            this.textBox4.TabIndex = 2;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 53);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(98, 13);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Numero de Factura";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 124);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(90, 13);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Codigo Empleado";
             // 
             // label3
             // 
@@ -188,6 +106,140 @@ namespace WindowsFormsApp1.FORMULARIOS
             this.label4.TabIndex = 6;
             this.label4.Text = "Codigo Detalle Compra";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Codigo Empleado";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Numero de Factura";
+            // 
+            // txtCodigoEmpleado
+            // 
+            this.txtCodigoEmpleado.Location = new System.Drawing.Point(119, 117);
+            this.txtCodigoEmpleado.Name = "txtCodigoEmpleado";
+            this.txtCodigoEmpleado.Size = new System.Drawing.Size(242, 20);
+            this.txtCodigoEmpleado.TabIndex = 3;
+            // 
+            // txtNumeroFactura
+            // 
+            this.txtNumeroFactura.Location = new System.Drawing.Point(119, 50);
+            this.txtNumeroFactura.Name = "txtNumeroFactura";
+            this.txtNumeroFactura.Size = new System.Drawing.Size(242, 20);
+            this.txtNumeroFactura.TabIndex = 2;
+            // 
+            // txtYEAR
+            // 
+            this.txtYEAR.Location = new System.Drawing.Point(529, 117);
+            this.txtYEAR.Name = "txtYEAR";
+            this.txtYEAR.Size = new System.Drawing.Size(57, 20);
+            this.txtYEAR.TabIndex = 1;
+            this.txtYEAR.Text = "yy";
+            this.txtYEAR.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtDetalleCompra
+            // 
+            this.txtDetalleCompra.Location = new System.Drawing.Point(516, 50);
+            this.txtDetalleCompra.Name = "txtDetalleCompra";
+            this.txtDetalleCompra.Size = new System.Drawing.Size(242, 20);
+            this.txtDetalleCompra.TabIndex = 0;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(664, 390);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(124, 37);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "NUEVO";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(424, 390);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(124, 37);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "ELIMINAR";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(230, 390);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(124, 37);
+            this.btnModificar.TabIndex = 2;
+            this.btnModificar.Text = "MODIFICAR";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 390);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "AGREGAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // datagridFacturaCompra
+            // 
+            this.datagridFacturaCompra.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.datagridFacturaCompra.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.datagridFacturaCompra.Location = new System.Drawing.Point(12, 191);
+            this.datagridFacturaCompra.Name = "datagridFacturaCompra";
+            this.datagridFacturaCompra.Size = new System.Drawing.Size(776, 184);
+            this.datagridFacturaCompra.TabIndex = 0;
+            this.datagridFacturaCompra.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.datagridFacturaCompra_CellContentClick);
+            // 
+            // txtMES
+            // 
+            this.txtMES.Location = new System.Drawing.Point(613, 117);
+            this.txtMES.Name = "txtMES";
+            this.txtMES.Size = new System.Drawing.Size(57, 20);
+            this.txtMES.TabIndex = 8;
+            this.txtMES.Text = "mm";
+            this.txtMES.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtDIA
+            // 
+            this.txtDIA.Location = new System.Drawing.Point(697, 117);
+            this.txtDIA.Name = "txtDIA";
+            this.txtDIA.Size = new System.Drawing.Size(57, 20);
+            this.txtDIA.TabIndex = 9;
+            this.txtDIA.Text = "dd";
+            this.txtDIA.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(592, 117);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(15, 24);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "/";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(676, 116);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(15, 24);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "/";
+            // 
             // FacturaCompra
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -196,10 +248,11 @@ namespace WindowsFormsApp1.FORMULARIOS
             this.Controls.Add(this.panel1);
             this.Name = "FacturaCompra";
             this.Text = "FacturaCompra";
+            this.Load += new System.EventHandler(this.FacturaCompra_Load);
             this.panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.datagridFacturaCompra)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.datagridFacturaCompra)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -213,13 +266,17 @@ namespace WindowsFormsApp1.FORMULARIOS
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtCodigoEmpleado;
+        private System.Windows.Forms.TextBox txtNumeroFactura;
+        private System.Windows.Forms.TextBox txtYEAR;
+        private System.Windows.Forms.TextBox txtDetalleCompra;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnModificar;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtDIA;
+        private System.Windows.Forms.TextBox txtMES;
     }
 }
--- WindowsFormsApp1/FORMULARIOS/FacturaCompra.cs
+++ WindowsFormsApp1/FORMULARIOS/FacturaCompra.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace WindowsFormsApp1.FORMULARIOS
 {
@@ -15,6 +17,127 @@ namespace WindowsFormsApp1.FORMULARIOS
         public FacturaCompra()
         {
             InitializeComponent();
+        }
+
+        private void FacturaCompra_Load(object sender, EventArgs e)
+        {
+            Conexion.Conectar();
+            MessageBox.Show("Conexion Exitosa");
+            datagridFacturaCompra.DataSource = llenar_Grid();
+        }
+
+        public DataTable llenar_Grid()
+        {
+            Conexion.Conectar();
+            DataTable dt = new DataTable();
+            string consulta = "SELECT NumFacturaCompra,IdEmpleado_FK,IdDetalleCompra_Fk,FechaCompra FROM [Punta_El_Cacique1].[dbo].[FacturaCompra] GO;";
+            SqlCommand cmd = new SqlCommand(consulta, Conexion.Conectar());
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            return dt;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string NumFacturaCompra = txtNumeroFactura.Text;
+            string IdEmpleado = txtCodigoEmpleado.Text;
+            string IdDetalleCompra = txtDetalleCompra.Text;
+            string year = txtYEAR.Text;
+            string mes = txtMES.Text;
+            string dia = txtDIA.Text;
+
+
+            if ((IdEmpleado.Equals("")) || (IdDetalleCompra.Equals("")) || (year.Equals("")) || (dia.Equals("")) || (mes.Equals("")) )
+            {
+                MessageBox.Show("Ingresa Datos Correctos", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+            else
+            {
+                if(year.Length < 4)
+                {
+                    MessageBox.Show("Ingresa el año con un formato de 4 digitos", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return;
+                }
+                int a = int.Parse(year);
+                int b = int.Parse(mes);
+                int c = int.Parse(dia);
+                DateTime fecha = new DateTime(a,b,c);
+                Conexion.Conectar();
+                string insertar = "INSERT INTO FacturaCompra (IdEmpleado_FK, IdDetalleCompra_Fk, FechaCompra) VALUES (@IdEmpleado_FK, @IdDetalleCompra_Fk, @FechaCompra)";
+                SqlCommand cmd1 = new SqlCommand(insertar, Conexion.Conectar());
+                cmd1.Parameters.AddWithValue("@IdEmpleado_FK", IdEmpleado);
+                cmd1.Parameters.AddWithValue("@IdDetalleCompra_Fk", IdDetalleCompra);
+                cmd1.Parameters.AddWithValue("@FechaCompra", fecha);
+                cmd1.ExecuteNonQuery();
+                MessageBox.Show("Registro agregado con exito!");
+                datagridFacturaCompra.DataSource = llenar_Grid();
+
+            }
+        }
+
+        private void datagridFacturaCompra_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                txtNumeroFactura.Text = datagridFacturaCompra.CurrentRow.Cells[0].Value.ToString();
+                txtCodigoEmpleado.Text = datagridFacturaCompra.CurrentRow.Cells[1].Value.ToString();
+                txtDetalleCompra.Text = datagridFacturaCompra.CurrentRow.Cells[2].Value.ToString();
+                string fecha_s = datagridFacturaCompra.CurrentRow.Cells[3].Value.ToString();
+                char delimitador = '/';
+                string[] valores = fecha_s.Split(delimitador);
+
+                // Setiando la year
+                string aux = valores[2];
+                string year = aux[0] + "" + aux[1] + "" + aux[2] + "" + aux[3];
+
+                txtYEAR.Text = year;
+                txtMES.Text = valores[1];
+                txtDIA.Text = valores[0];
+
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string NumFacturaCompra = txtNumeroFactura.Text;
+            string IdEmpleado = txtCodigoEmpleado.Text;
+            string IdDetalleCompra = txtDetalleCompra.Text;
+            string year = txtYEAR.Text;
+            string mes = txtMES.Text;
+            string dia = txtDIA.Text;
+            if ( (NumFacturaCompra.Equals("")) || (IdEmpleado.Equals("")) || (IdDetalleCompra.Equals("")) || (year.Equals("")) || (dia.Equals("")) || (mes.Equals("")))
+            {
+                MessageBox.Show("Ingresa Datos Correctos", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+            else
+            {
+                if (year.Length < 4)
+                {
+                    MessageBox.Show("Ingresa el año con un formato de 4 digitos", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return;
+                }
+                int a = int.Parse(year);
+                int b = int.Parse(mes);
+                int c = int.Parse(dia);
+                DateTime fecha = new DateTime(a, b, c);
+
+                Conexion.Conectar();
+                string actualizar = "UPDATE FacturaCompra SET  FechaCompra = @FechaCompra WHERE NumFacturaCompra = @NumFacturaCompra";
+                SqlCommand cmd2 = new SqlCommand(actualizar, Conexion.Conectar());
+                cmd2.Parameters.AddWithValue("@NumFacturaCompra", NumFacturaCompra);
+                cmd2.Parameters.AddWithValue("FechaCompra", fecha);
+                cmd2.ExecuteNonQuery();
+                datagridFacturaCompra.DataSource = llenar_Grid();
+                MessageBox.Show("Registro Actualizado con exito!");
+
+            }
+
         }
     }
 }
