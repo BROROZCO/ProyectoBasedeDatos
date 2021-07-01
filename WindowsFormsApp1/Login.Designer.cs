84078358eb060c360c4ad181f5113be81bbd982b
Invisible <unenes.viejos@gmail.com>
2021-06-30 19:56:24 -0600

    Login Hecho faltan pasar permisos

---
+++ WindowsFormsApp1/Login.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.Usuario = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnIniciarSesion = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtPassword);
+            this.panel1.Controls.Add(this.btnIniciarSesion);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.Usuario);
+            this.panel1.Controls.Add(this.txtUser);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(456, 411);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(46, 122);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(351, 20);
+            this.txtUser.TabIndex = 1;
+            this.txtUser.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Usuario
+            // 
+            this.Usuario.AutoSize = true;
+            this.Usuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Usuario.Location = new System.Drawing.Point(173, 60);
+            this.Usuario.Name = "Usuario";
+            this.Usuario.Size = new System.Drawing.Size(96, 29);
+            this.Usuario.TabIndex = 2;
+            this.Usuario.Text = "Usuario";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(156, 164);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Contraseña";
+            // 
+            // btnIniciarSesion
+            // 
+            this.btnIniciarSesion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIniciarSesion.Location = new System.Drawing.Point(152, 279);
+            this.btnIniciarSesion.Name = "btnIniciarSesion";
+            this.btnIniciarSesion.Size = new System.Drawing.Size(149, 39);
+            this.btnIniciarSesion.TabIndex = 4;
+            this.btnIniciarSesion.Text = "INICIAR";
+            this.btnIniciarSesion.UseVisualStyleBackColor = true;
+            this.btnIniciarSesion.Click += new System.EventHandler(this.btnIniciarSesion_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(46, 230);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(351, 20);
+            this.txtPassword.TabIndex = 5;
+            this.txtPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(456, 411);
+            this.Controls.Add(this.panel1);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnIniciarSesion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Usuario;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPassword;
+    }
+}
---
+++ WindowsFormsApp1/Login.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp1
+{
+    public partial class Login : Form
+    {
+        string user = "";
+        string pass = "";
+        string estado = "";
+        string puesto = "";
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciarSesion_Click(object sender, EventArgs e)
+        {
+            string a = txtUser.Text;
+            string b = txtPassword.Text;
+            login(a, b);
+        }
+
+
+        public void login(string user_, string pass_)
+        {
+            try
+            {
+                Conexion.Conectar();
+                MessageBox.Show("Conexion Exitosa");
+                string consul = "SELECT UserName ,Password_, CuentaEstado, IdPuesto_FK  FROM Empleado WHERE UserName = @UserName AND Password_ = @Password_  ";
+                SqlCommand cmd = new SqlCommand(consul, Conexion.Conectar());
+                cmd.Parameters.AddWithValue("@UserName", user_);
+                cmd.Parameters.AddWithValue("@Password_", pass_);
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+
+                if (dt.Rows.Count == 1)
+                {
+                    user = dt.Rows[0][0].ToString();
+                    pass = dt.Rows[0][1].ToString();
+                    estado = dt.Rows[0][2].ToString();
+                    puesto = dt.Rows[0][3].ToString();
+
+                    MessageBox.Show("Usuario: "+ user + " pass: "+pass+" Estado: "+estado+ " ID PUESTO: "+puesto);
+
+                }
+                else
+                {
+                    MessageBox.Show("Usuario o Contraseña incorrecto");
+                }
+
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            finally
+            {
+
+            }
+        }
+    }
+}
